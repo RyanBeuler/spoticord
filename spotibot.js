b175d433d5bdd24ef64e53f90ecee1f38fa8c6bf
RyanBeuler <ryanbeuler@gmail.com>
2017-11-27 00:11:46 -0500

    added play text

--- spotibot.js
+++ spotibot.js
@@ -1,14 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-var lame = require('lame');
-var Speaker = require('speaker');
-var Spotify = require('spotify-web');
-var uri = process.argv[2] || 'spotify:track:1N8xmBWy10c2w1U8ZgPxtG';
 
-// Spotify credentials...
-var username = process.env.USERNAME;
-var password = process.env.PASSWORD;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -45,22 +38,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'Pong!'
                 });
             case 'play':
-                Spotify.login(username, password, function (err, spotify) {
-                  if (err) throw err;
-                  // first get a "Track" instance from the track URI
-                  spotify.get(uri, function (err, track) {
-                    if (err) throw err;
-                    console.log('Playing: %s - %s', track.artist[0].name, track.name);
-                    // play() returns a readable stream of MP3 audio data
-                    track.play()
-                      .pipe(new lame.Decoder())
-                      .pipe(new Speaker())
-                      .on('finish', function () {
-                        spotify.disconnect();
-                      });
-
-                  });
-                });
                 bot.sendMessage({
                     to: channelID,
                     message: 'Playing Spotify track!'
