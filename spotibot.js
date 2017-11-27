5c893bf7c605a6abe592c5e32da4fbea5d98206e
RyanBeuler <ryanbeuler@gmail.com>
2017-11-26 23:45:45 -0500

    force commit

--- spotibot.js
+++ spotibot.js
@@ -1,6 +1,14 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var lame = require('lame');
+var Speaker = require('speaker');
+var Spotify = require('spotify-web');
+var uri = process.argv[2] || 'spotify:track:1N8xmBWy10c2w1U8ZgPxtG';
+
+// Spotify credentials...
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -35,6 +43,30 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
+                });
+            case 'play':
+
+                Spotify.login(username, password, function (err, spotify) {
+                  if (err) throw err;
+
+                  // first get a "Track" instance from the track URI
+                  spotify.get(uri, function (err, track) {
+                    if (err) throw err;
+                    console.log('Playing: %s - %s', track.artist[0].name, track.name);
+
+                    // play() returns a readable stream of MP3 audio data
+                    track.play()
+                      .pipe(new lame.Decoder())
+                      .pipe(new Speaker())
+                      .on('finish', function () {
+                        spotify.disconnect();
+                      });
+
+                  });
+                });
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Playing Spotify track!'
                 });
             break;
             // Just add any case commands if you want to..
