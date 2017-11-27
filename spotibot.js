93843ead97cfed2aa834ab28e73b2f099a3a59b0
RyanBeuler <ryanbeuler@gmail.com>
2017-11-27 00:39:48 -0500

    moved token auth

--- spotibot.js
+++ spotibot.js
@@ -23,6 +23,29 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri : 'http://www.example.com/callback'
 });
 
+// First retrieve an access token
+spotifyApi.authorizationCodeGrant(authorizationCode)
+  .then(function(data) {
+    console.log('Retrieved access token', data.body['access_token']);
+    // Set the access token
+    spotifyApi.setAccessToken(data.body['access_token']);
+    // Use the access token to retrieve information about the user connected to it
+    return spotifyApi.getMe();
+  })
+  .then(function(data) {
+    // "Retrieved data for Faruk Sahin"
+    console.log('Retrieved data for ' + data.body['display_name']);
+    // "Email is farukemresahin@gmail.com"
+    console.log('Email is ' + data.body.email);
+    // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
+    console.log('Image URL is ' + data.body.images[0].url);
+    // "This user has a premium account"
+    console.log('This user has a ' + data.body.product + ' account');
+  })
+  .catch(function(err) {
+    console.log('Something went wrong', err.message);
+  });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -44,28 +67,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Playing Spotify track!'
                 });
-                // First retrieve an access token
-                spotifyApi.authorizationCodeGrant(authorizationCode)
-                  .then(function(data) {
-                    console.log('Retrieved access token', data.body['access_token']);
-                    // Set the access token
-                    spotifyApi.setAccessToken(data.body['access_token']);
-                    // Use the access token to retrieve information about the user connected to it
-                    return spotifyApi.getMe();
-                  })
-                  .then(function(data) {
-                    // "Retrieved data for Faruk Sahin"
-                    console.log('Retrieved data for ' + data.body['display_name']);
-                    // "Email is farukemresahin@gmail.com"
-                    console.log('Email is ' + data.body.email);
-                    // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
-                    console.log('Image URL is ' + data.body.images[0].url);
-                    // "This user has a premium account"
-                    console.log('This user has a ' + data.body.product + ' account');
-                  })
-                  .catch(function(err) {
-                    console.log('Something went wrong', err.message);
-                  });
             break;
             // Just add any case commands if you want to..
          }
